2cf5ec95e81996b3694eb68b808697f94740f083
Sam Bail <sam@superconductive.com>
2020-11-06 00:23:21 +0100

    Some more readme and tweaks to the example DAG

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # great_expectations_airflow (experimental as of 11/05/2020)
 
 An Airflow operator for Great Expectations (greatexpectations.io), a Python library for testing and validating data.
+
+Install with `pip install great_expectations_airflow` and then import `GreatExpectationsOperator` in your DAG file.
+
+Pre-requisites: Install `great_expectations` and `apache-airflow`, of course.
+
+See the **examples** directory for an example DAG with some sample tasks that demonstrate operator functionality. The example DAG file contains a comment with instructions on how to run the examples.
+
+**This operator is in very early stages of development! Feel free to submit issues, PRs, or ping the current author (Sam Bail) in the Great Expectations Slack for feedback.
--- examples/example_great_expectations_dag.py
+++ examples/example_great_expectations_dag.py
@@ -6,6 +6,7 @@
 # Note: The tasks that don't set an explicit data_context_root_dir need to be run from within
 # this examples directory, otherwise GE won't know where to find the data context
 
+import os
 import airflow
 from airflow import DAG
 from great_expectations_airflow.operators.great_expectations import GreatExpectationsOperator
@@ -19,7 +20,6 @@ dag = DAG(
     dag_id='example_great_expectations_dag',
     default_args=default_args
 )
-
 
 # This runs a checkpoint that will pass
 ge_checkpoint_pass = GreatExpectationsOperator(
@@ -46,13 +46,13 @@ ge_checkpoint_fail_but_continue = GreatExpectationsOperator(
     dag=dag
 )
 
-
 # This runs an expectation suite against a data asset that passes the tests
+data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/yellow_tripdata_sample_2019-01.csv')
 ge_batch_kwargs_pass = GreatExpectationsOperator(
     task_id='ge_batch_kwargs_pass',
     expectation_suite_name='taxi.demo',
     batch_kwargs={
-        'path': '/Users/sam/code/ge_demo/data/yellow_tripdata_sample_2019-01.csv',
+        'path': data_file,
         'datasource': 'data__dir'
     },
     dag=dag
@@ -64,7 +64,7 @@ ge_batch_kwargs_list_pass = GreatExpectationsOperator(
     assets_to_validate=[
         {
             'batch_kwargs': {
-                'path': '/Users/sam/code/ge_demo/data/yellow_tripdata_sample_2019-01.csv',
+                'path': data_file,
                 'datasource': 'data__dir'
             },
             'expectation_suite_name': 'taxi.demo'
@@ -74,12 +74,16 @@ ge_batch_kwargs_list_pass = GreatExpectationsOperator(
 )
 
 # This runs a checkpoint and passes in a root dir
+ge_root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'great_expectations')
 ge_checkpoint_pass_root_dir = GreatExpectationsOperator(
     task_id='ge_checkpoint_pass_root_dir',
     run_name='ge_airflow_run',
     checkpoint_name='taxi.pass.chk',
-    data_context_root_dir='./great_expectations',
+    data_context_root_dir=ge_root_dir,
     dag=dag
 )
 
 # TODO: example for creating an in-memory data context using a dictionary config
+
+# Just a little dependency here to run a couple of tasks in sequence, this isn't meaningful in any way
+ge_checkpoint_pass >> ge_checkpoint_fail_but_continue
