9a486a641207e45c7ac7564bae647a97e862dae3
petedejoy <pete@astronomer.io>
2020-11-12 22:22:42 -0600

    Restructure repo and update readme

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="great_expectations_airflow",
+    name="airflow-provider-great-expectations",
     version="0.0.1",
-    description="An Airflow operator for Great Expectations",
+    description="An Airflow provider for Great Expectations",
     url="https://github.com/great-expectations/great_expectations_airflow",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
