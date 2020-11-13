ba52ed34f47f3176965d77920f175fae88bbddec
Pete DeJoy <32076749+petedejoy@users.noreply.github.com>
2020-11-13 10:28:20 -0600

    Update setup.py
    Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setuptools.setup(
     name="airflow-provider-great-expectations",
     version="0.0.1",
     description="An Airflow provider for Great Expectations",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/great-expectations/great_expectations_airflow",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
