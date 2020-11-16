c70e763c3030df46a2cc0a924d3213e94238d65a
Sam Bail <sam@superconductive.com>
2020-11-16 10:35:18 +0100

    Small tweaks to setup.py meta

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="airflow-provider-great-expectations",
     version="0.0.1",
+    author="Great Expectations",
     description="An Apache Airflow provider for Great Expectations",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="https://github.com/great-expectations/great_expectations_airflow",
+    url="https://github.com/great-expectations/airflow-provider-great-expectations",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     package_data={'great_expectations_provider': ['examples/data/*.csv']},
