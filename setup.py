5b801faee9f8d3d392a14aa0ea4512f1668a5033
Sam Bail <sam@superconductive.com>
2020-11-16 11:18:38 +0100

    Version change because pypi...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="airflow-provider-great-expectations",
-    version="0.0.1",
+    version="0.0.2",
     author="Great Expectations",
     description="An Apache Airflow provider for Great Expectations",
     long_description=long_description,
