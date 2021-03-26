0e69d8d7e06385ff6e158411c5e073f398230101
Pete DeJoy <32076749+petedejoy@users.noreply.github.com>
2021-03-26 13:56:32 -0400

    Update great_expectations_provider/__init__.py
    Co-authored-by: Sam Bail <samanthapbail@gmail.com>

--- great_expectations_provider/__init__.py
+++ great_expectations_provider/__init__.py
@@ -2,6 +2,6 @@ def get_provider_info():
     return {
         "package-name": "airflow-provider-great-expectations",
         "name": "Great Expectations Provider",
-        "description": "An Apache Airflow provider for GE.",
+        "description": "An Apache Airflow provider for Great Expectations.",
         "versions": ["0.0.4"]
     }
