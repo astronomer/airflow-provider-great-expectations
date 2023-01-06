37603d91778a3ca7a63cb0ad13472bbab8c3832f
TJaniF <tamara.fingerlin@astronomer.io>
2023-01-06 13:44:05 +0100

    actually catching the error

--- great_expectations_provider/operators/great_expectations.py
+++ great_expectations_provider/operators/great_expectations.py
@@ -245,8 +245,8 @@ class GreatExpectationsOperator(BaseOperator):
         elif conn_type == "snowflake":
             try:  # auto generated connection kwargs Snowflake provider >=4.0.0
                 uri_string = f"snowflake://{self.conn.login}:{self.conn.password}@{self.conn.extra_dejson['account']}.{self.conn.extra_dejson['region']}/{self.conn.extra_dejson['database']}/{schema}?warehouse={self.conn.extra_dejson['warehouse']}&role={self.conn.extra_dejson['role']}"  # noqa
-            except AirflowProviderDeprecationWarning as ex:  # auto generated connection kwargs Snowflake provider < 4.0.0
-                self.log.info(ex)
+            except KeyError:  # auto generated connection kwargs Snowflake provider < 4.0.0
+                self.log.warning("Snowflake provider >=4.0.0 updated the Snowflake hook to not use the extra prefix.")
                 uri_string = f"snowflake://{self.conn.login}:{self.conn.password}@{self.conn.extra_dejson['extra__snowflake__account']}.{self.conn.extra_dejson['extra__snowflake__region']}/{self.conn.extra_dejson['extra__snowflake__database']}/{schema}?warehouse={self.conn.extra_dejson['extra__snowflake__warehouse']}&role={self.conn.extra_dejson['extra__snowflake__role']}"
         elif conn_type == "gcpbigquery":
             uri_string = f"{self.conn.host}{schema}"
