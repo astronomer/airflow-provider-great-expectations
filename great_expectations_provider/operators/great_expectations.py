09dbdac47ec5c71e67bd41e01772070f1d9315a6
Tamara Janina Fingerlin <90063506+TJaniF@users.noreply.github.com>
2023-08-17 15:16:42 +0200

    Fix make_connection_configuration for MSSQL (adding a driver) (#113)

--- great_expectations_provider/operators/great_expectations.py
+++ great_expectations_provider/operators/great_expectations.py
@@ -239,6 +239,7 @@ class GreatExpectationsOperator(BaseOperator):
     def make_connection_configuration(self) -> Dict[str, str]:
         """Builds connection strings based off existing Airflow connections. Only supports necessary extras."""
         uri_string = ""
+        driver = ""
         if not self.conn:
             raise ValueError(f"Connections does not exist in Airflow for conn_id: {self.conn_id}")
         self.schema = self.schema or self.conn.schema
@@ -247,11 +248,21 @@ class GreatExpectationsOperator(BaseOperator):
             odbc_connector = ""
             if conn_type in ("redshift", "postgres"):
                 odbc_connector = "postgresql+psycopg2"
+                database_name = self.schema
             elif conn_type == "mysql":
                 odbc_connector = "mysql"
-            else:
+                database_name = self.schema
+            elif conn_type == "mssql":
                 odbc_connector = "mssql+pyodbc"
-            uri_string = f"{odbc_connector}://{self.conn.login}:{self.conn.password}@{self.conn.host}:{self.conn.port}/{self.schema}"  # noqa
+                ms_driver = self.conn.extra_dejson.get("driver") or "ODBC Driver 17 for SQL Server"
+                driver = f"?driver={ms_driver}"
+                database_name = self.conn.schema or "master"
+            else:
+                raise ValueError(f"Conn type: {conn_type} is not supported.")
+            uri_string = (
+                f"{odbc_connector}://{self.conn.login}:{self.conn.password}@"
+                f"{self.conn.host}:{self.conn.port}/{database_name}{driver}"
+            )
         elif conn_type == "snowflake":
             try:
                 return self.build_snowflake_connection_config_from_hook()
