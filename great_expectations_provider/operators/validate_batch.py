47220cee060793f456f9af08deb79dfd81595ef4
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-12-13 16:49:35 +0100

    add ValidateBatchOperator

---
+++ great_expectations_provider/operators/validate_batch.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+from typing import Callable, Literal, TYPE_CHECKING
+
+from airflow.models import BaseOperator
+
+
+if TYPE_CHECKING:
+    from great_expectations.data_context import AbstractDataContext
+    from great_expectations.core.batch import BatchParameters
+    from great_expectations.core.batch_definition import BatchDefinition
+    from great_expectations.expectations import Expectation
+    from great_expectations import ExpectationSuite
+    from airflow.utils.context import Context
+
+
+class ValidateBatchOperator(BaseOperator):
+    def __init__(
+        self,
+        configure_batch_definition: Callable[[AbstractDataContext], BatchDefinition],
+        expect: Expectation | ExpectationSuite,
+        batch_parameters: BatchParameters | None = None,
+        context_type: Literal["ephemeral", "cloud"] = "ephemeral",
+        result_format: Literal["BOOLEAN_ONLY", "BASIC", "SUMMARY", "COMPLETE"]
+        | None = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        import great_expectations as gx
+
+        if batch_parameters is None:
+            self.batch_parameters = {}
+        else:
+            self.batch_parameters = batch_parameters
+
+        self.context = gx.get_context(mode=context_type)
+        self.batch_definition = configure_batch_definition(self.context)
+        self.expect = expect
+        self.result_format = result_format
+
+    def execute(self, context: Context) -> dict:
+        batch = self.batch_definition.get_batch(batch_parameters=self.batch_parameters)
+        if self.result_format:
+            result = batch.validate(
+                expect=self.expect, result_format=self.result_format
+            )
+        else:
+            result = batch.validate(expect=self.expect)
+        print(result)
+        return result.to_json_dict()
