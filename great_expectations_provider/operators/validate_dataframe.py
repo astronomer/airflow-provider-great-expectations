a11fb61bd0e871e6c68d20b6ef9bf605121a4574
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-12-13 21:11:23 +0100

    ValidateDataFrameOperator and tests

---
+++ great_expectations_provider/operators/validate_dataframe.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from typing import Callable, Literal, TYPE_CHECKING
+
+from airflow.models import BaseOperator
+
+
+if TYPE_CHECKING:
+    from great_expectations.expectations import Expectation
+    from great_expectations import ExpectationSuite
+    from airflow.utils.context import Context
+    from pandas import DataFrame
+
+
+class ValidateDataframeOperator(BaseOperator):
+    def __init__(
+        self,
+        task_id: str,
+        configure_dataframe: Callable[[], DataFrame],
+        expect: Expectation | ExpectationSuite,
+        result_format: Literal["BOOLEAN_ONLY", "BASIC", "SUMMARY", "COMPLETE"]
+        | None = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs, task_id=task_id)
+        import great_expectations as gx
+
+        self.context = gx.get_context(mode="ephemeral")
+        self.dataframe = configure_dataframe()
+        self.expect = expect
+        self.result_format = result_format
+
+    def execute(self, context: Context) -> dict:
+        batch = (
+            self.context.data_sources.add_pandas(name=self.task_id)
+            .add_dataframe_asset(name=self.task_id)
+            .add_batch_definition_whole_dataframe(name=self.task_id)
+            .get_batch(
+                batch_parameters={
+                    "dataframe": self.dataframe,
+                }
+            )
+        )
+        if self.result_format:
+            result = batch.validate(
+                expect=self.expect, result_format=self.result_format
+            )
+        else:
+            result = batch.validate(expect=self.expect)
+        print(result)
+        return result.to_json_dict()
---
+++ tests/operators/test_validate_dataframe_operator.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from great_expectations import ExpectationSuite
+from great_expectations.expectations import ExpectColumnValuesToBeInSet
+from great_expectations_provider.operators.validate_dataframe import (
+    ValidateDataframeOperator,
+)
+from great_expectations.core import (
+    ExpectationValidationResult,
+    ExpectationSuiteValidationResult,
+)
+
+
+class TestValidateDataframeOperator:
+    def test_expectation(self):
+        # arrange
+        column_name = "col_A"
+
+        def configure_dataframe() -> pd.DataFrame:
+            return pd.DataFrame({column_name: ["a", "b", "c"]})
+
+        expect = ExpectColumnValuesToBeInSet(
+            column=column_name, value_set=["a", "b", "c", "d", "e"]
+        )
+
+        validate_batch = ValidateDataframeOperator(
+            task_id="validate_batch_success",
+            configure_dataframe=configure_dataframe,
+            expect=expect,
+        )
+
+        # act
+        result = validate_batch.execute(context={})
+
+        # assert
+        deserialized_result = ExpectationValidationResult(**result)
+        assert deserialized_result.success
+
+    def test_expectation_suite(self):
+        # arrange
+        column_name = "col_A"
+
+        def configure_dataframe() -> pd.DataFrame:
+            return pd.DataFrame({column_name: ["a", "b", "c"]})
+
+        expect = ExpectationSuite(
+            name="test suite",
+            expectations=[
+                ExpectColumnValuesToBeInSet(
+                    column=column_name, value_set=["a", "b", "c", "d", "e"]
+                ),
+            ],
+        )
+
+        validate_batch = ValidateDataframeOperator(
+            task_id="validate_batch_success",
+            configure_dataframe=configure_dataframe,
+            expect=expect,
+        )
+
+        # act
+        result = validate_batch.execute(context={})
+
+        # assert
+        deserialized_result = ExpectationSuiteValidationResult(**result)
+        assert deserialized_result.success
