3f452af3852c3ead4bcb88eecb7e08a4e154a7de
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-01-09 14:35:34 +0100

    test df path

--- tests/integration/test_validate_batch_operator.py
+++ tests/integration/test_validate_batch_operator.py
@@ -6,41 +6,45 @@ import great_expectations as gx
 from great_expectations import expectations as gxe
 from great_expectations.data_context import AbstractDataContext
 from great_expectations.datasource.fluent.interfaces import Batch
+import pandas as pd
 
-
-def rand_name() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k=10))
+from great_expectations_provider.operators.validate_batch import GXValidateBatchOperator
+from integration.conftest import rand_name
+from great_expectations.core.batch_definition import BatchDefinition
 
 
 class TestValidateBatchOperator:
     COL_NAME = "my_column"
 
-    def test_validate_dataframe(self):
-        ...
+    def test_validate_dataframe(self, ensure_data_source_cleanup) -> None:
+        task_id = f"validate_batch_dataframe_integration_test_{rand_name()}"
+        ensure_data_source_cleanup(task_id)
+        dataframe = pd.DataFrame({self.COL_NAME: ["a", "b", "c"]})
+        expect = gxe.ExpectColumnValuesToBeInSet(
+            column=self.COL_NAME,
+            value_set=["a", "b", "c", "d", "e"],  # type: ignore[arg-type]
+        )
+        batch_parameters = {"dataframe": dataframe}
 
-    def test_validate_csv(self):
-        ...
-
-    def test_validate_sql(self):
-        ...
-
-
-    @pytest.fixture
-    def configure_batch(
-        self,
-        ensure_data_source_cleanup: Callable[[str], None],
-    ) -> Callable[[AbstractDataContext], gx.Batch]:
-        def _configure_batch(context: AbstractDataContext) -> gx.Checkpoint:
-            batch_definition = (
-                context.data_sources.add_pandas(name=rand_name())
-                .add_dataframe_asset(rand_name())
-                .add_batch_definition_whole_dataframe(rand_name())
+        def configure_batch_definition(context: AbstractDataContext) -> BatchDefinition:
+            return (
+                context.data_sources.add_pandas(name=task_id)
+                .add_dataframe_asset(task_id)
+                .add_batch_definition_whole_dataframe(task_id)
             )
 
+        validate_cloud_batch = GXValidateBatchOperator(
+            task_id=task_id,
+            configure_batch_definition=configure_batch_definition,
+            expect=expect,
+            batch_parameters=batch_parameters,
+            context_type="cloud",
+        )
 
-            ensure_data_source_cleanup(batch_definition.data_asset.datasource.name)
+        result = validate_cloud_batch.execute(context={})
 
-            return batch_definition.get_batch()
+        assert result["success"] is True
 
-        return _configure_batch
+    def test_validate_csv(self): ...
 
+    def test_validate_sql(self): ...
