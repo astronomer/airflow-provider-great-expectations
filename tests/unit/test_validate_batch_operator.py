a1501aee5be9308bf28ff97f9685ee4101e8c25a
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-01-10 14:27:12 +0100

    test common, remove redundant tests

--- tests/unit/test_validate_batch_operator.py
+++ tests/unit/test_validate_batch_operator.py
@@ -13,7 +13,6 @@ from great_expectations.data_context import AbstractDataContext
 from great_expectations.expectations import (
     ExpectColumnValuesToBeInSet,
 )
-from pytest_mock import MockerFixture
 
 from great_expectations_provider.operators.validate_batch import GXValidateBatchOperator
 
@@ -219,65 +218,3 @@ class TestValidateBatchOperator:
 
         # assert
         mock_gx.get_context.assert_called_once_with(mode=context_type)
-
-    def test_batch_parameters(self, mock_gx: Mock):
-        """Expect that param batch_parameters is passed to BatchDefinition.get_batch"""
-        # arrange
-        mock_context = mock_gx.get_context.return_value
-        mock_validation_definition = (
-            mock_context.validation_definitions.add_or_update.return_value
-        )
-        mock_batch_definition = Mock()
-        expect = Mock()
-        batch_parameters = {
-            "year": "2024",
-            "month": "01",
-            "day": "01",
-        }
-
-        validate_batch = GXValidateBatchOperator(
-            task_id="validate_batch_success",
-            configure_batch_definition=lambda context: mock_batch_definition,
-            expect=expect,
-            batch_parameters=batch_parameters,
-            context_type="ephemeral",
-        )
-
-        # act
-        validate_batch.execute(context={})
-
-        # assert
-        mock_validation_definition.run.assert_called_once_with(
-            batch_parameters=batch_parameters
-        )
-
-    def test_validation_definition_construction(self, mock_gx: Mock):
-        """Expect that the expect param, the task_id, and the return value
-        of the configure_batch_definition param are used to construct a ValidationDefinition.
-        """
-        # arrange
-        task_id = "test_validation_definition_construction"
-        mock_context = mock_gx.get_context.return_value
-        mock_validation_definition_factory = mock_context.validation_definitions
-        mock_validation_definition = mock_gx.ValidationDefinition.return_value
-        mock_batch_definition = Mock()
-        expect = Mock()
-
-        validate_batch = GXValidateBatchOperator(
-            task_id=task_id,
-            configure_batch_definition=lambda context: mock_batch_definition,
-            expect=expect,
-            batch_parameters=Mock(),
-            context_type="ephemeral",
-        )
-
-        # act
-        validate_batch.execute(context={})
-
-        # assert
-        mock_gx.ValidationDefinition.assert_called_once_with(
-            name=task_id, suite=expect, data=mock_batch_definition
-        )
-        mock_validation_definition_factory.add_or_update.assert_called_once_with(
-            validation=mock_validation_definition
-        )
