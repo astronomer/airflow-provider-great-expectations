cdec342f3a64083bff7136cb82d63f90afeace36
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-01-10 10:19:14 +0100

    update to use validation definition

--- great_expectations_provider/operators/validate_dataframe.py
+++ great_expectations_provider/operators/validate_dataframe.py
@@ -7,6 +7,8 @@ from airflow.models import BaseOperator
 if TYPE_CHECKING:
     from airflow.utils.context import Context
     from great_expectations import ExpectationSuite
+    from great_expectations.core.batch_definition import BatchDefinition
+    from great_expectations.data_context import AbstractDataContext
     from great_expectations.expectations import Expectation
     from pandas import DataFrame
 
@@ -33,20 +35,47 @@ class GXValidateDataFrameOperator(BaseOperator):
         import great_expectations as gx
 
         gx_context = gx.get_context(mode=self.context_type)
-        batch = (
-            gx_context.data_sources.add_pandas(name=self.task_id)
-            .add_dataframe_asset(name=self.task_id)
-            .add_batch_definition_whole_dataframe(name=self.task_id)
-            .get_batch(
-                batch_parameters={
-                    "dataframe": self.dataframe,
-                }
-            )
+        batch_definition = self._get_pandas_batch_definition(gx_context)
+        batch_parameters = {
+            "dataframe": self.dataframe,
+        }
+        if isinstance(self.expect, gx.expectations.Expectation):
+            suite = gx.ExpectationSuite(name=self.task_id, expectations=[self.expect])
+        else:
+            suite = self.expect
+        validation_definition = gx_context.validation_definitions.add_or_update(
+            validation=gx.ValidationDefinition(
+                name=self.task_id,
+                suite=suite,
+                data=batch_definition,
+            ),
         )
         if self.result_format:
-            result = batch.validate(
-                expect=self.expect, result_format=self.result_format
+            result = validation_definition.run(
+                batch_parameters=batch_parameters,
+                result_format=self.result_format,
             )
         else:
-            result = batch.validate(expect=self.expect)
+            result = validation_definition.run(
+                batch_parameters=batch_parameters,
+            )
+
         return result.describe_dict()
+
+    def _get_spark_batch_definition(
+        self, gx_context: AbstractDataContext
+    ) -> BatchDefinition:
+        return (
+            gx_context.data_sources.add_or_update_spark(name=self.task_id)
+            .add_dataframe_asset(name=self.task_id)
+            .add_batch_definition_whole_dataframe(name=self.task_id)
+        )
+
+    def _get_pandas_batch_definition(
+        self, gx_context: AbstractDataContext
+    ) -> BatchDefinition:
+        return (
+            gx_context.data_sources.add_or_update_pandas(name=self.task_id)
+            .add_dataframe_asset(name=self.task_id)
+            .add_batch_definition_whole_dataframe(name=self.task_id)
+        )
