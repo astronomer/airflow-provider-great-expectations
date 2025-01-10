76038e4f23263859158c931e879b1ee9ca279ebc
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-01-10 19:43:38 +0100

    docstring

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -8,6 +8,13 @@ from pytest_mock import MockerFixture
 
 @pytest.fixture
 def mock_gx(mocker: MockerFixture) -> Generator[Mock, None, None]:
+    """Due to constraints from Airflow, GX must be imported locally
+    within the Operator, which makes mocking the GX namespace difficult.
+    This fixture allows us to globally patch GX.
+
+    One known issue with this approach is that isinstance checks fail against
+    mocks.
+    """
     mock_gx = Mock()
     mock_gx.expectations.Expectation = Expectation  # required for isinstance check
     mocker.patch.dict("sys.modules", {"great_expectations": mock_gx})
