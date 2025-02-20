f3016621ff989adb44b75b0fb672898bbd0895ec
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-02-20 14:48:06 +0100

    update GXCloudConnection

--- great_expectations_provider/hooks/gx_cloud.py
+++ great_expectations_provider/hooks/gx_cloud.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from functools import cached_property
 from typing import Any
 
 from airflow.hooks.base import BaseHook
+
+from great_expectations_provider.common.gx_context_actions import load_data_context
 
 
 @dataclass(frozen=True)
@@ -21,17 +22,17 @@ class GXCloudConnection(BaseHook):
     :param token: GX cloud access token.
     """
 
-    conn_name_attr = "gx_cloud_config_id"
+    conn_name_attr = "gx_cloud_conn_id"
     default_conn_name = "gx_cloud_default"
     conn_type = "gx_cloud"
     hook_name = "Great Expectations Cloud"
 
-    def __init__(self, gx_cloud_config_id: str = default_conn_name):
+    def __init__(self, gx_cloud_conn_id: str = default_conn_name):
         super().__init__()
-        self.gx_cloud_config_id = gx_cloud_config_id
+        self.gx_cloud_conn_id = gx_cloud_conn_id
 
     def get_conn(self) -> GXCloudConfig:  # type: ignore[override]
-        config = self.get_connection(self.gx_cloud_config_id)
+        config = self.get_connection(self.gx_cloud_conn_id)
         return GXCloudConfig(
             cloud_access_token=config.password, cloud_organization_id=config.login
         )
@@ -50,14 +51,8 @@ class GXCloudConnection(BaseHook):
 
     def test_connection(self) -> tuple[bool, str]:
         try:
-            config = self.get_connection(self.gx_cloud_config_id)
-            import great_expectations as gx
-
-            gx.get_context(
-                mode="cloud",
-                cloud_access_token=config.password,
-                cloud_organization_id=config.login,
-            )
+            config = self.get_conn()
+            load_data_context(context_type="cloud", gx_cloud_config=config)
             return True, "Connection successful."
         except Exception as error:
             return False, str(error)
