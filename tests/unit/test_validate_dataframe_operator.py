db8912112ccabf9492d21f6b984e4150be7a6242
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-02-27 16:49:58 -0500

    Unit test

--- tests/unit/test_validate_dataframe_operator.py
+++ tests/unit/test_validate_dataframe_operator.py
@@ -8,6 +8,12 @@ from great_expectations import ExpectationSuite
 from great_expectations.core import ExpectationValidationResult
 from great_expectations.expectations import ExpectColumnValuesToBeInSet
 from great_expectations.datasource.fluent import PandasDatasource, SparkDatasource
+from great_expectations.datasource.fluent.pandas_datasource import (
+    DataFrameAsset as PandasDataFrameAsset,
+)
+from great_expectations.datasource.fluent.spark_datasource import (
+    DataFrameAsset as SparkDataFrameAsset,
+)
 from great_expectations.data_context import EphemeralDataContext
 
 from great_expectations_provider.common.constants import USER_AGENT_STR
@@ -16,6 +22,14 @@ from great_expectations_provider.operators.validate_dataframe import (
 )
 
 pytestmark = pytest.mark.unit
+
+
+class MockSparkDataFrame:
+    # Dummy class so we don't have to have pyspark dep in unit tests
+    ...
+
+
+MockSparkDataFrame.__name__ = "DataFrame"
 
 
 class TestValidateDataFrameOperator:
@@ -202,12 +216,185 @@ class TestValidateDataFrameOperator:
             user_agent_str=USER_AGENT_STR,
         )
 
+    def test_pandas_does_not_error_when_no_datasource(
+        self, mock_gx_no_datasource: Mock
+    ) -> None:
+        validate_df = GXValidateDataFrameOperator(
+            task_id="validate_df_success",
+            configure_dataframe=Mock(return_value=Mock(spec=pd.DataFrame)),
+            expect=Mock(),
+            context_type="cloud",
+        )
+
+        # act
+        validate_df.execute(context={})
+
+    def test_pandas_does_not_error_when_no_asset(
+        self, mock_gx_with_pandas_datasource_but_no_asset: Mock
+    ) -> None:
+        validate_df = GXValidateDataFrameOperator(
+            task_id="validate_df_success",
+            configure_dataframe=Mock(return_value=Mock(spec=pd.DataFrame)),
+            expect=Mock(),
+            context_type="cloud",
+        )
+
+        # act
+        validate_df.execute(context={})
+
+    def test_pandas_does_not_error_when_no_batch_definition(
+        self, mock_gx_with_pandas_datasource_but_no_batch_definition: Mock
+    ) -> None:
+        validate_df = GXValidateDataFrameOperator(
+            task_id="validate_df_success",
+            configure_dataframe=Mock(return_value=Mock(spec=pd.DataFrame)),
+            expect=Mock(),
+            context_type="cloud",
+        )
+
+        # act
+        validate_df.execute(context={})
+
+    def test_spark_does_not_error_when_no_datasource(
+        self, mock_gx_no_datasource: Mock
+    ) -> None:
+        validate_df = GXValidateDataFrameOperator(
+            task_id="validate_df_success",
+            configure_dataframe=Mock(return_value=Mock(spec=pd.DataFrame)),
+            expect=Mock(),
+            context_type="cloud",
+        )
+
+        # act
+        validate_df.execute(context={})
+
+    def test_spark_does_not_error_when_no_asset(
+        self, mock_gx_with_spark_datasource_but_no_asset: Mock
+    ) -> None:
+        validate_df = GXValidateDataFrameOperator(
+            task_id="validate_df_success",
+            configure_dataframe=Mock(return_value=MockSparkDataFrame()),
+            expect=Mock(),
+            context_type="cloud",
+        )
+
+        # act
+        validate_df.execute(context={})
+
+    def test_spark_does_not_error_when_no_batch_definition(
+        self, mock_gx_with_spark_datasource_but_no_batch_definition: Mock
+    ) -> None:
+        validate_df = GXValidateDataFrameOperator(
+            task_id="validate_df_success",
+            configure_dataframe=Mock(return_value=MockSparkDataFrame()),
+            expect=Mock(),
+            context_type="cloud",
+        )
+
+        # act
+        validate_df.execute(context={})
+
+    @pytest.fixture
+    def mock_gx_no_datasource(
+        self,
+        mock_gx: Mock,
+    ) -> Mock:
+        mock_datasource = create_autospec(spec=PandasDatasource)
+        mock_datasource.get_asset.side_effect = LookupError
+        mock_context = create_autospec(
+            spec=EphemeralDataContext,
+            data_sources=Mock(get=Mock(side_effect=KeyError)),
+        )
+        mock_gx.get_context.return_value = mock_context
+        return mock_gx
+
     @pytest.fixture
     def mock_gx_with_pandas_datasource(
         self,
         mock_gx: Mock,
     ) -> Mock:
         mock_datasource = create_autospec(spec=PandasDatasource)
+        mock_context = create_autospec(
+            spec=EphemeralDataContext,
+            data_sources=Mock(get=Mock(return_value=mock_datasource)),
+        )
+        mock_gx.get_context.return_value = mock_context
+        return mock_gx
+
+    @pytest.fixture
+    def mock_gx_with_pandas_datasource_but_no_asset(
+        self,
+        mock_gx: Mock,
+    ) -> Mock:
+        mock_datasource = create_autospec(spec=PandasDatasource)
+        mock_datasource.get_asset.side_effect = LookupError
+        mock_context = create_autospec(
+            spec=EphemeralDataContext,
+            data_sources=Mock(get=Mock(return_value=mock_datasource)),
+        )
+        mock_gx.get_context.return_value = mock_context
+        return mock_gx
+
+    @pytest.fixture
+    def mock_gx_with_pandas_datasource_but_no_batch_definition(
+        self,
+        mock_gx: Mock,
+    ) -> Mock:
+        mock_asset = create_autospec(
+            spec=PandasDataFrameAsset,
+            get_batch_definition=Mock(side_effect=KeyError),
+        )
+        mock_datasource = create_autospec(
+            spec=PandasDatasource,
+            get_asset=Mock(return_value=mock_asset),
+        )
+        mock_context = create_autospec(
+            spec=EphemeralDataContext,
+            data_sources=Mock(get=Mock(return_value=mock_datasource)),
+        )
+        mock_gx.get_context.return_value = mock_context
+        return mock_gx
+
+    @pytest.fixture
+    def mock_gx_with_spark_datasource(
+        self,
+        mock_gx: Mock,
+    ) -> Mock:
+        mock_datasource = create_autospec(spec=SparkDatasource)
+        mock_context = create_autospec(
+            spec=EphemeralDataContext,
+            data_sources=Mock(get=Mock(return_value=mock_datasource)),
+        )
+        mock_gx.get_context.return_value = mock_context
+        return mock_gx
+
+    @pytest.fixture
+    def mock_gx_with_spark_datasource_but_no_asset(
+        self,
+        mock_gx: Mock,
+    ) -> Mock:
+        mock_datasource = create_autospec(spec=SparkDatasource)
+        mock_datasource.get_asset.side_effect = LookupError
+        mock_context = create_autospec(
+            spec=EphemeralDataContext,
+            data_sources=Mock(get=Mock(return_value=mock_datasource)),
+        )
+        mock_gx.get_context.return_value = mock_context
+        return mock_gx
+
+    @pytest.fixture
+    def mock_gx_with_spark_datasource_but_no_batch_definition(
+        self,
+        mock_gx: Mock,
+    ) -> Mock:
+        mock_asset = create_autospec(
+            spec=SparkDataFrameAsset,
+            get_batch_definition=Mock(side_effect=KeyError),
+        )
+        mock_datasource = create_autospec(
+            spec=SparkDatasource,
+            get_asset=Mock(return_value=mock_asset),
+        )
         mock_context = create_autospec(
             spec=EphemeralDataContext,
             data_sources=Mock(get=Mock(return_value=mock_datasource)),
