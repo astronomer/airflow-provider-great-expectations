513472b97d17927e83c96743e0ae3dca25bebf97
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-06-18 10:03:16 +0200

    update example dags

--- great_expectations_provider/example_dags/example_great_expectations_dag.py
+++ great_expectations_provider/example_dags/example_great_expectations_dag.py
@@ -4,9 +4,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 import great_expectations.expectations as gxe
-import pandas as pd
 from airflow import DAG
-from airflow.decorators import task
 from airflow.models.baseoperator import chain
 from great_expectations import Checkpoint, ExpectationSuite, ValidationDefinition
 
@@ -21,6 +19,7 @@ from great_expectations_provider.operators.validate_dataframe import (
 if TYPE_CHECKING:
     from great_expectations.core.batch_definition import BatchDefinition
     from great_expectations.data_context import AbstractDataContext
+    import pandas as pd
 
 base_path = Path(__file__).parents[2]
 data_dir = base_path / "include" / "data"
@@ -41,6 +40,12 @@ def configure_pandas_batch_definition(context: AbstractDataContext) -> BatchDefi
         regex=r"yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2}).csv",
     )
     return batch_definition
+
+
+def configure_data_frame() -> pd.DataFrame:
+    import pandas as pd
+
+    return pd.read_csv(data_file)
 
 
 def configure_checkpoint(context: AbstractDataContext) -> Checkpoint:
@@ -99,6 +104,7 @@ FAILURE_MONTH = "02"
 batch_parameters = {"year": "2019", "month": SUCCESS_MONTH}
 
 
+# define a consistent set of expectations we'll use throughout the pipeline
 expectation_suite = ExpectationSuite(
     name="Taxi Data Expectations",
     expectations=[
@@ -117,8 +123,6 @@ expectation_suite = ExpectationSuite(
 with DAG(
     dag_id="gx_provider_example_dag",
 ) as dag:
-    # define a consistent set of expectations we'll use throughout the pipeline
-
     validate_extract = GXValidateBatchOperator(
         task_id="validate_extract",
         configure_batch_definition=configure_pandas_batch_definition,
@@ -126,21 +130,11 @@ with DAG(
         batch_parameters=batch_parameters,
     )
 
-    @task.short_circuit()
-    def check_validate_extract(task_instance):
-        result = task_instance.xcom_pull(task_ids="validate_extract")
-        return result.get("success")
-
     validate_transform = GXValidateDataFrameOperator(
         task_id="validate_transform",
-        configure_dataframe=lambda: pd.read_csv(data_file),
+        configure_dataframe=configure_data_frame,
         expect=expectation_suite,
     )
-
-    @task.short_circuit()
-    def check_validate_transform(task_instance):
-        result = task_instance.xcom_pull(task_ids="validate_transform")
-        return result.get("success")
 
     validate_load = GXValidateCheckpointOperator(
         task_id="validate_load",
@@ -148,16 +142,8 @@ with DAG(
         batch_parameters=batch_parameters,
     )
 
-    @task.short_circuit()
-    def check_validate_load(task_instance):
-        result = task_instance.xcom_pull(task_ids="validate_load")
-        return result.get("success")
-
     chain(
         validate_extract,
-        check_validate_extract(),  # type: ignore[call-arg, misc]
         validate_transform,
-        check_validate_transform(),  # type: ignore[call-arg, misc]
         validate_load,
-        check_validate_load(),  # type: ignore[call-arg, misc]
     )
