5e47a72542a5cfd68a40237c0b9ce71a75dea596
Pankaj Koti <pankajkoti699@gmail.com>
2025-07-09 17:22:58 +0530

    Release 1.0.0a4

--- great_expectations_provider/__init__.py
+++ great_expectations_provider/__init__.py
@@ -1,4 +1,4 @@
 # TODO: Update the .github/scripts/verify_tag_and_version.py script so that we can use the VERSION from the
 #  great_expectation_provider/common/constants.py file.
 
-__version__ = "1.0.0a3"
+__version__ = "1.0.0a4"
